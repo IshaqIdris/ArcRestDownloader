2eaf02093d04b86e68d95620319b06a39aaf3566
Ishaq Idris <sharkyferrari@gmail.com>
2017-12-18 16:59:48 +1300

    Added complimentary print statements/ comments
    Should make the program easier to understand or see what areas are not functioning

--- arcrestDownloaderTemp.py
+++ arcrestDownloaderTemp.py
@@ -15,6 +15,7 @@ def download_features(fs_url, date, out_path):
     out_folder_path = cwd + '/Shapefiles'
     out_name = "test.gdb"
     arcpy.CreateFileGDB_management(out_folder_path, out_name)
+    print ("Creating a empty GDB....")
     
     '''downloads a hosted service features into a feature class'''
     agol_securityHandler = AGOLTokenSecurityHandler('Username','Password','http://company.maps.arcgis.com/')
@@ -25,19 +26,28 @@ def download_features(fs_url, date, out_path):
     ldf = LayerDefinitionFilter()
 
     #CHANGE CONDITION AFTER THE "and" TO QUERY SOMETHING ELSE OTHER THAN DATE
-    #e.g. "ref='EBN_BDP_00....'"
+    #e.g. "ref='EBN_BDP_006_TAP'"
+    #Date format mm/dd/yyyy
     ldf.addFilter(0, where="1=1" and "date> date\'" + date + "\'")
+    print ("Querying data....")
     print (fs.query(layerDefsFilter=ldf,returnCountOnly=True))
 
     queryResults = fs.query(layerDefsFilter=ldf,returnCountOnly=False,returnGeometry=True)
     result = queryResults[0].save(r'in_memory','SampleCities')
+    print ("Saved results....")
     arcpy.CopyFeatures_management(result,out_path)
+    print ("Features exported")
+    print ("All done!")
 
- 
+print("Welcome to the ArcRestDownloader!")
+print("Please make sure you have added a valid date in the LastDate.txt")
+
 #Read through text file for date filter and file counter
+print("Reading LastDate.txt....")
 f = open('C:/Users/ishaq.idris/Documents/Shapefiles/LastDate.txt', 'r')
 f.readline()    #skip line
 date = f.readline()
+print("Only downloading data from " + date + "onwards....")
 f.readline()    #skip line
 counter = f.readline() #ONLY USED FOR FILE NAMING PUSPOSES
 
@@ -46,6 +56,7 @@ now = datetime.datetime.now()
 #Output folder CHANGE THIS TO DESIRED DESTINATION OF FILES
 folder = "Documents/Shapefiles/{0}{1}{2}_{3}f/".format(now.year, "0"*(2-len(str(now.month)))+str(now.month), "0"*(2-len(str(now.day)))+str(now.day), counter)
 if not os.path.isdir(folder):
+    print("Making output folder at " + folder + "....")
     os.makedirs(folder)
 f.close()
 
@@ -53,11 +64,13 @@ f.close()
 lines = open('LastDate.txt', 'r').readlines()
 print ("length " + str(len(lines)))
 counter = int(counter) + 1
+print("Counter incremented....")
 lines[len(lines)-1] = str(counter)
 open('LastDate.txt', 'w').writelines(lines)
 f.close()
 
 #CHANGE THE VALUE IN QUOTATIONS TO CHANGE OUTPUT FILES NAME
+cwd = os.getcwd() #gets current working directory of this python file(not required)
 out_path = folder + 'test.gdb/shapes'
 
 #This is the feature service URL CHANGE THIS TO THE DESIRED FEATURE SERVICE URL
